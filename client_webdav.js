2b238444f09c83cb13b923b80aed68d83f098f61
cobertos <me+git@cobertos.com>
2022-06-06 02:28:08 -0400

    Add cors_anywhere

--- client_webdav.js
+++ client_webdav.js
@@ -1,6 +1,7 @@
-var wfs = require("webdav-fs")("http://127.0.0.1:8000", null, null);
+const { createAdapter } = require("webdav-fs");
+const wfs = createAdapter("http://127.0.0.1:7769/http://127.0.0.1:8000", {});
 
-wfs.readdir("/level1/", function(err, c){
+wfs.readdir("/", "stat", function(err, c){
     if(err){ console.error(err); }
     else{ console.log(c); }
-}, 'node');
+});
--- server_webdav.js
+++ server_webdav.js
@@ -1,6 +1,15 @@
-var jsDAV = require("jsDAV");
+const jsDAV = require("jsDAV");
+const cors_proxy = require('cors-anywhere');
 //jsDAV.debugMode = true;
 
 jsDAV.createServer({
     node: __dirname + "/data"
 }, 8000);
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    //requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(7769, '0.0.0.0', function() {
+    console.log('Running CORS Anywhere on 0.0.0.0:7769');
+});
