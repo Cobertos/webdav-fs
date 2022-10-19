cacd8cfe289ff329b0fa40b606284f94fe68485c
cobertos <me+git@cobertos.com>
2022-10-19 00:02:08 -0400

    Update to use webdav (the parent repo)

--- client_webdav.js
+++ client_webdav.js
@@ -1,7 +1,18 @@
-const { createAdapter } = require("webdav-fs");
-const wfs2 = createAdapter("http://127.0.0.1:8000/somewhere/", {});
+// const { createAdapter } = require("webdav-fs");
+// const wfs2 = createAdapter("http://127.0.0.1:8000/somewhere/", {});
 
-wfs2.readdir("level1/", "stat", function(err, c){
-    if(err){ console.error(err); }
-    else{ console.log(c); }
-});
+// wfs2.readdir("level1/", "stat", function(err, c){
+//     if(err){ console.error(err); }
+//     else{ console.log(c); }
+// });
+
+const { createClient } = require("webdav");
+
+const client = createClient(
+    "http://127.0.0.1:8000/somewhere/",
+);
+async function t(){
+    const directoryItems = await client.getDirectoryContents("level1/");
+    console.log(directoryItems);
+}
+t();
